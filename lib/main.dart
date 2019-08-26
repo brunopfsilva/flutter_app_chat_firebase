7b71c2a763a6b639c9444e89803538928bab255b
bruno pf silva <bruno.pfsilva@hotmail.com>
2019-08-26 15:59:13 +0100

    criando layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_chat_firebase/MyHomePage.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-
-
-}
-
-
-
+void main() => runApp(MaterialApp(
+  home: MyHomePage(),
+));
