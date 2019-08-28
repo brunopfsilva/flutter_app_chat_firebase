5572e1d86d2fc873d5d969d88c488e0721e312ff
bruno pf silva <bruno.pfsilva@hotmail.com>
2019-08-28 20:49:37 +0100

    firebase configurado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_chat_firebase/MyHomePage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(MaterialApp(
-  home: MyHomePage(),
-));
+void main() {
+
+//  Firestore.instance.collection("teste").document("teste").setData({"teste" : "teste"});
+
+  runApp(MaterialApp(home: MyHomePage()));
+
+}
